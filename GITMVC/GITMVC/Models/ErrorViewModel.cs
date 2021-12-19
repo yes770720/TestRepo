423ca41dca1f56567c766c06ad3fd5553ad0ec47
yes770720 <33674875+yes770720@users.noreply.github.com>
2021-12-19 17:50:12 +0800

    首次置入專案

---
+++ GITMVC/GITMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GITMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
